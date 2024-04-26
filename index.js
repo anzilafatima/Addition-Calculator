9b7b2a4a13cf076153d4fdebf64f6ac8cced20b1
anzilafatima <anzilafatima205@gmail.com>
2024-04-27 01:43:26 +0500

    addition-calculator

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt({
+    name: "num1",
+    type: "number",
+    message: "kindly enter your  first number",
+});
+const input2 = await inquirer.prompt({
+    name: "num2",
+    type: "number",
+    message: "kindly enter your second number",
+});
+let total = input1.num1 + input2.num2;
+console.log(total);
